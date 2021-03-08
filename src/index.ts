72d3e5ec1372d04702b61e66e014881374999cce
serializedowen <wjh199455@gmail.com>
2021-03-08 16:27:16 +0800

    feat(version): v 0.1.0

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * returnType of reflected property
+ */
+export const returnType = 'design:returntype';
+/**
+ * type of reflected property
+ */
+export const type = 'design:type';
+
+/**
+ * intake paramTypes of reflected property
+ */
+export const paramType = 'design:paramTypes';
+
+export default {
+  returnType,
+  type,
+  paramType,
+};
